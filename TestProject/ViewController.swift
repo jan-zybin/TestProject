ab9a40493a8af8d5d6a46dc7d9ecec8d33656b8e
jan-zybin <jan.zybin@gmail.com>
2016-03-15 12:23:13 +0200

    Initial Commit

---
+++ TestProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestProject
+//
+//  Created by Jan Zybin on 3/15/16.
+//  Copyright © 2016 Jan Zybin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
